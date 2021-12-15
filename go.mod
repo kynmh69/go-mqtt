712135c72e0240396145c82faaa873d672923a52
kynmh69 <hiroki.kayanuma@gmail.com>
2021-12-15 18:57:51 +0900

    mod import

---
+++ go.mod
@@ -0,0 +1,10 @@
+module mod
+
+go 1.17
+
+require github.com/eclipse/paho.mqtt.golang v1.3.5
+
+require (
+	github.com/gorilla/websocket v1.4.2 // indirect
+	golang.org/x/net v0.0.0-20211215060638-4ddde0e984e9 // indirect
+)
--- src/subscriber.go
+++ src/subscriber.go
@@ -1,1 +1,45 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+)
+
+func main() {
+	fmt.Println("開始")
+	msgCh := make(chan mqtt.Message)
+	var fn mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
+		msgCh <- msg
+		fmt.Println("msgCh", msgCh)
+	}
+
+	opts := mqtt.NewClientOptions()
+	opts.AddBroker("tcp://broker.emqx.io:1883")
+	cc := mqtt.NewClient(opts)
+
+	if token := cc.Connect(); token.Wait() && token.Error() != nil {
+		log.Fatalf("Mqtt error: %s", token.Error())
+	}
+
+	if subscribeToken := cc.Subscribe("go-mqtt/sample", 0, fn); subscribeToken.Wait() && subscribeToken.Error() != nil {
+		log.Fatal(subscribeToken.Error())
+	}
+
+	signalCh := make(chan os.Signal, 1)
+	signal.Notify(signalCh, os.Interrupt)
+
+	for {
+		select {
+		case m := <-msgCh:
+			fmt.Printf("topic: %v, payload: %v\n", m.Topic(), string(m.Payload()))
+		case <-signalCh:
+			fmt.Printf("Interrupt detected.\n")
+			cc.Disconnect(1000)
+			return
+		}
+	}
+}
